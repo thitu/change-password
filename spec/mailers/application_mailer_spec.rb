a8f53d0762dc003b3a6ebfc11f7fe895373a0cd0
Nikolas Thitu <nikolas@hauz.io>
2020-05-11 22:43:01 -0400

    spec update

--- spec/mailers/application_mailer_spec.rb
+++ spec/mailers/application_mailer_spec.rb
@@ -3,5 +3,17 @@
 require('rails_helper')
 
 RSpec.describe(ApplicationMailer, type: :mailer) do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'notify' do
+    ENV['MAILER_MSG'] = 'your password was changed'
+    let(:send_notification) { ApplicationMailer.notify('a@b.com') }
+
+    it 'renders the headers' do
+      notification = send_notification
+
+      expect(notification.subject).to(eq('Password was successfully changed'))
+      expect(notification.message.to_s).to(include(ENV['MAILER_MSG']))
+      expect(notification.from).to(eq([ENV['MAILER_FROM']]))
+      expect(notification.to).to(eq(['a@b.com']))
+    end
+  end
 end
