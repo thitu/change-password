a50c34f23b965e646149c2fd33ecc58181bd37aa
Nikolas Thitu <nikolas@hauz.io>
2020-05-11 22:55:01 -0400

    spec update

--- spec/requests/patch_request_spec.rb
+++ spec/requests/patch_request_spec.rb
@@ -2,24 +2,25 @@
 
 require('rails_helper')
 
-RSpec.describe('POST requests', type: :request) do
+RSpec.describe('PATCH requests', type: :request) do
   it 'will redirect to the index if CSRF validation fails' do
     allow_any_instance_of(ActionController::Base).to(receive(:protect_against_forgery?).and_return(true))
 
-    post '/'
+    patch '/'
     expect(response).to(redirect_to('/'))
     follow_redirect!
 
     expect(response).to(render_template(:index))
   end
 
-  it 'responds with not found if specified id is not authorized' do
-    post '/', params: { id: 'bob@bob.com' }
-    expect(response).to(be_not_found)
+  it 'responds with forbidden if session is *not* authorized' do
+    patch '/', params: { id: 'user1@bob.com', pw_0: '12345678', pw_1: '12345678', intent: 'authorize' }
+    expect(response).to(be_forbidden)
   end
 
-  it 'responds with success if specified id is authorized' do
+  it 'responds with success if session is authorized' do
     post '/', params: { id: 'user1@bob.com' }
+    patch '/', params: { id: 'user1@bob.com', pw_0: '12345678', pw_1: '12345678', intent: 'authorize' }
     expect(response).to(be_successful)
   end
 end
